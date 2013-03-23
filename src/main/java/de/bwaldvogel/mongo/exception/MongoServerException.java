47d7eb0e80cbb3548b807d5513caadd7d8e68c99
Benedikt Waldvogel <mail@bwaldvogel.de>
2013-03-23 03:41:37 +0100

    prevent empty error messages

--- src/main/java/de/bwaldvogel/mongo/exception/MongoServerException.java
+++ src/main/java/de/bwaldvogel/mongo/exception/MongoServerException.java
@@ -1,12 +1,14 @@
 package de.bwaldvogel.mongo.exception;
 
-
 public class MongoServerException extends Exception {
 
     private static final long serialVersionUID = 3357301041846925271L;
 
     public MongoServerException(String message) {
         super(message);
+        if (message == null || message.isEmpty()) {
+            throw new IllegalArgumentException("illegal error message");
+        }
     }
 
     public MongoServerException(String message, Throwable cause) {
