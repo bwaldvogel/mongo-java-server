4e47245b67be24db9d112da29f49271cd16eb723
Benedikt Waldvogel <mail@bwaldvogel.de>
2014-02-23 16:36:01 +0100

    cleanup and avoid deprecation warning

--- src/main/java/de/bwaldvogel/mongo/wire/MongoExceptionHandler.java
+++ src/main/java/de/bwaldvogel/mongo/wire/MongoExceptionHandler.java
@@ -1,13 +1,11 @@
 package de.bwaldvogel.mongo.wire;
 
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MongoExceptionHandler extends ChannelInboundHandlerAdapter implements ChannelHandler {
+public class MongoExceptionHandler extends ChannelInboundHandlerAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(MongoExceptionHandler.class);
 
