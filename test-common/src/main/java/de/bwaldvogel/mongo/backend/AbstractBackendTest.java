3ad56b6653e86caf6237e165f654a4c7336b06f7
Benedikt Waldvogel <mail@bwaldvogel.de>
2016-03-06 12:38:39 +0100

    cleanup

--- test-common/src/main/java/de/bwaldvogel/mongo/backend/AbstractBackendTest.java
+++ test-common/src/main/java/de/bwaldvogel/mongo/backend/AbstractBackendTest.java
@@ -21,6 +21,7 @@ import static org.fest.assertions.Fail.fail;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.net.InetSocketAddress;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -48,6 +49,8 @@ import org.bson.BsonTimestamp;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,8 +61,10 @@ import com.mongodb.MongoException;
 import com.mongodb.MongoNamespace;
 import com.mongodb.MongoQueryException;
 import com.mongodb.MongoWriteException;
+import com.mongodb.ServerAddress;
 import com.mongodb.WriteConcern;
 import com.mongodb.async.SingleResultCallback;
+import com.mongodb.async.client.MongoClients;
 import com.mongodb.bulk.BulkWriteResult;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -81,9 +86,81 @@ import com.mongodb.client.model.WriteModel;
 import com.mongodb.client.result.DeleteResult;
 import com.mongodb.client.result.UpdateResult;
 
-public abstract class AbstractBackendTest extends AbstractSimpleBackendTest {
+import de.bwaldvogel.mongo.MongoBackend;
+import de.bwaldvogel.mongo.MongoServer;
+
+public abstract class AbstractBackendTest {
 
     private static final Logger log = LoggerFactory.getLogger(AbstractBackendTest.class);
+
+    private MongoServer mongoServer;
+
+    protected com.mongodb.MongoClient syncClient;
+    protected com.mongodb.async.client.MongoClient asyncClient;
+
+    protected MongoDatabase db;
+    protected MongoCollection<Document> collection;
+
+    protected com.mongodb.async.client.MongoCollection<Document> asyncCollection;
+
+    protected Document runCommand(String commandName) {
+        return runCommand(new Document(commandName, Integer.valueOf(1)));
+    }
+
+    protected Document runCommand(Document command) {
+        return getAdminDb().runCommand(command);
+    }
+
+    protected MongoCollection<Document> getCollection(String collectionName) {
+        return db.getCollection(collectionName);
+    }
+
+    protected MongoDatabase getAdminDb() {
+        return syncClient.getDatabase("admin");
+    }
+
+    protected abstract MongoBackend createBackend() throws Exception;
+
+    @Before
+    public void setUp() throws Exception {
+        spinUpServer();
+    }
+
+    @After
+    public void tearDown() {
+        shutdownServer();
+    }
+
+    protected void spinUpServer() throws Exception {
+        MongoBackend backend = createBackend();
+        mongoServer = new MongoServer(backend);
+        InetSocketAddress serverAddress = mongoServer.bind();
+        syncClient = new com.mongodb.MongoClient(new ServerAddress(serverAddress));
+        asyncClient = MongoClients.create("mongodb://" + serverAddress.getHostName() + ":" + serverAddress.getPort());
+        db = syncClient.getDatabase("testdb");
+        collection = db.getCollection("testcoll");
+
+        MongoNamespace namespace = collection.getNamespace();
+        com.mongodb.async.client.MongoDatabase asyncDb = asyncClient.getDatabase(namespace.getDatabaseName());
+        asyncCollection = asyncDb.getCollection(namespace.getCollectionName());
+    }
+
+    protected void shutdownServer() {
+        syncClient.close();
+        asyncClient.close();
+        mongoServer.shutdownNow();
+    }
+
+    @Test
+    public void testSimpleInsert() throws Exception {
+        collection.insertOne(json("_id: 1"));
+    }
+
+    @Test
+    public void testSimpleInsertDelete() throws Exception {
+        collection.insertOne(json("_id: 1"));
+        collection.deleteOne(json("_id: 1"));
+    }
 
     @Test
     public void testCreateCollection() throws Exception {
