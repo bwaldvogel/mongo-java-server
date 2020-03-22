3ddc69d9a3c4b336448ac6a2c27c4066826d2a18
Benedikt Waldvogel <mail@bwaldvogel.de>
2020-03-22 13:47:00 +0100

    Cleanup InMemoryMongoServer

--- memory-backend/src/main/java/de/bwaldvogel/mongo/InMemoryMongoServer.java
+++ memory-backend/src/main/java/de/bwaldvogel/mongo/InMemoryMongoServer.java
@@ -9,16 +9,13 @@ public class InMemoryMongoServer extends MongoServer {
 
     private static final Logger log = LoggerFactory.getLogger(InMemoryMongoServer.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         final MongoServer mongoServer = new InMemoryMongoServer();
         mongoServer.bind("localhost", 27017);
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                log.info("shutting down {}", mongoServer);
-                mongoServer.shutdownNow();
-            }
-        });
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("shutting down {}", mongoServer);
+            mongoServer.shutdownNow();
+        }));
     }
 
     /**
