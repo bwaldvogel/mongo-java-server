a11c5d3840ba26b98711b9ede727bf05c5503912
Benedikt Waldvogel <mail@bwaldvogel.de>
2020-04-19 11:17:44 +0200

    Extend InMemoryCursorTest

--- core/src/test/java/de/bwaldvogel/mongo/backend/InMemoryCursorTest.java
+++ core/src/test/java/de/bwaldvogel/mongo/backend/InMemoryCursorTest.java
@@ -3,9 +3,10 @@ package de.bwaldvogel.mongo.backend;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 
@@ -33,18 +34,21 @@ class InMemoryCursorTest {
 
     @Test
     void testTakeDocuments() {
-        List<Document> documents = Arrays.asList(new Document("name", "Joe"), new Document("name", "Mary"));
+        List<Document> documents = IntStream.range(0, 22)
+            .mapToObj(idx -> new Document("number", idx + 1))
+            .collect(Collectors.toList());
 
-        InMemoryCursor cursor1 = new InMemoryCursor(1L, documents);
-        assertThat(cursor1.takeDocuments(10)).containsExactlyElementsOf(documents);
-
-        InMemoryCursor cursor2 = new InMemoryCursor(1L, documents);
-        assertThat(cursor2.takeDocuments(1)).containsExactly(documents.get(0));
-        assertThat(cursor2.takeDocuments(1)).containsExactly(documents.get(1));
-        assertThat(cursor2.takeDocuments(1)).isEmpty();
+        InMemoryCursor cursor = new InMemoryCursor(1L, documents);
+        assertThat(cursor.takeDocuments(1)).containsExactly(documents.get(0));
+        assertThat(cursor.takeDocuments(2)).containsExactly(documents.get(1), documents.get(2));
+        assertThat(cursor.takeDocuments(1)).containsExactly(documents.get(3));
+        assertThat(cursor.takeDocuments(6)).containsExactlyElementsOf(documents.subList(4, 10));
+        assertThat(cursor.takeDocuments(10)).containsExactlyElementsOf(documents.subList(10, 20));
+        assertThat(cursor.takeDocuments(10)).containsExactly(documents.get(20), documents.get(21));
+        assertThat(cursor.takeDocuments(10)).isEmpty();
 
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> cursor2.takeDocuments(0))
+            .isThrownBy(() -> cursor.takeDocuments(0))
             .withMessage("Illegal number to return: 0");
     }
 
