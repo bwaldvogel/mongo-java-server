683e9003b692cf20aed1be895eba450c68baac86
Benedikt Waldvogel <mail@bwaldvogel.de>
2020-04-19 11:52:13 +0200

    Cleanup QueryOperator

--- core/src/main/java/de/bwaldvogel/mongo/backend/QueryOperator.java
+++ core/src/main/java/de/bwaldvogel/mongo/backend/QueryOperator.java
@@ -27,7 +27,7 @@ public enum QueryOperator {
     TYPE("$type"),
     ;
 
-    private String value;
+    private final String value;
 
     QueryOperator(String value) {
         this.value = value;
@@ -47,11 +47,11 @@ public enum QueryOperator {
     }
 
     static QueryOperator fromValue(String value) throws MongoServerError {
-        QueryOperator op = MAP.get(value);
-        if (op == null) {
+        QueryOperator operator = MAP.get(value);
+        if (operator == null) {
             throw new BadValueException("unknown operator: " + value);
         }
-        return op;
+        return operator;
     }
 
 }
