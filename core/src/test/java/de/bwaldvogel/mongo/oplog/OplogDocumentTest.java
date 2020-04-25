0d2da45dfd7742ecab1e6f36938b2ead3478cd88
Benedikt Waldvogel <mail@bwaldvogel.de>
2020-04-25 18:27:56 +0200

    Improve OplogDocumentTest

--- core/src/test/java/de/bwaldvogel/mongo/oplog/OplogDocumentTest.java
+++ core/src/test/java/de/bwaldvogel/mongo/oplog/OplogDocumentTest.java
@@ -8,43 +8,60 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import de.bwaldvogel.mongo.bson.Document;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
 
 class OplogDocumentTest {
 
-    OplogDocument oplogDocument;
-    OplogDocument oplogDocument1;
+    private OplogDocument oplogDocument1;
+    private OplogDocument oplogDocument2;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         UUID uuid = UUID.randomUUID();
-        oplogDocument = new OplogDocument().withOperationDocument(new Document())
-            .withAdditionalOperationalDocument(new Document()).withT(123L).withHash(123L).withT(234L).withUUID(uuid);
-        oplogDocument1 = new OplogDocument().withOperationDocument(new Document())
-            .withAdditionalOperationalDocument(new Document()).withT(123L).withHash(123L).withT(234L).withUUID(uuid);
+
+        oplogDocument1 = new OplogDocument()
+            .withOperationDocument(new Document())
+            .withAdditionalOperationalDocument(new Document())
+            .withT(123L).withHash(123L).withT(234L).withUUID(uuid);
+
+        oplogDocument2 = new OplogDocument()
+            .withOperationDocument(new Document())
+            .withAdditionalOperationalDocument(new Document())
+            .withT(123L).withHash(123L).withT(234L).withUUID(uuid);
     }
 
     @Test
     void testEqualsNull() {
-        assertThat(oplogDocument).isNotEqualTo(null);
+        assertThat(oplogDocument1).isNotEqualTo(null);
     }
 
     @Test
     void testEqualsSameInstance() {
-        assertThat(oplogDocument).isEqualTo(oplogDocument);
+        assertThat(oplogDocument1).isEqualTo(oplogDocument1);
     }
 
     @Test
     void testEqualsDifferentType() {
-        assertThat(oplogDocument).isNotEqualTo(new Object());
+        assertThat(oplogDocument1).isNotEqualTo(new Object());
     }
 
     @Test
     void testEqualsUsingDocumentEquals() {
-        assertThat(oplogDocument).isEqualTo(oplogDocument1);
+        assertThat(oplogDocument1).isEqualTo(oplogDocument2);
+    }
+
+    @Test
+    void testEqualsHashCodeContract() throws Exception {
+        EqualsVerifier.forClass(OplogDocument.class)
+            .suppress(Warning.STRICT_INHERITANCE)
+            .withNonnullFields("document")
+            .verify();
     }
 
     @Test
     void testHashCode() {
-        assertThat(oplogDocument.hashCode()).isEqualTo(oplogDocument.asDocument().hashCode());
+        assertThat(oplogDocument1).hasSameHashCodeAs(oplogDocument1.asDocument());
     }
+
 }
