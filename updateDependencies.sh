fbb42d6d1b15ae14d5b6dd0837a302f735e986c0
Benedikt Waldvogel <mail@bwaldvogel.de>
2022-07-15 16:23:00 +0200

    Fix updateDependencies.sh

--- updateDependencies.sh
+++ updateDependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./gradlew --refresh-dependencies mongo-java-server-{core,test-common,{h2,memory,postgres}-backend}:dependencies --update-locks '*:*'
+./gradlew --refresh-dependencies dependencies mongo-java-server-{core,test-common,{h2,memory,postgres}-backend}:dependencies --update-locks '*:*'
