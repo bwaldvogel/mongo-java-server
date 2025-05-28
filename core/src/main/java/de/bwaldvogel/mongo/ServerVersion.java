af534dde0edd445b8dbbc80c6ac35975a833c8bd
Daniel <daniel.zappold@gmx.de>
2025-05-28 14:14:00 +0200

    Bump default wire protocol version: 7 -> 8

--- core/src/main/java/de/bwaldvogel/mongo/ServerVersion.java
+++ core/src/main/java/de/bwaldvogel/mongo/ServerVersion.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public enum ServerVersion implements MongoVersion {
     MONGO_3_6(List.of(3, 6, 0), 6),
-    MONGO_4_0(List.of(4, 0, 0), 7);
+    MONGO_4_0(List.of(4, 0, 0), 7),
+    MONGO_5_0(List.of(5, 0, 0), 8);
 
     private final List<Integer> versionArray;
     private final int wireVersion;
